8b48358ee0fbc4be75a8cd081006de8774da7d97
Jaydeep <khachariyajaydeep@gmail.com>
2023-05-26 00:52:37 +0530

    Make navbar

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`max-w-[1440px] mx-auto ${inter.className}`}>
         <Navbar />
         {children}
         <Footer />
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,24 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+import navImage from "../app/resources/images/faviconForNavbar.svg";
+import { BsGridFill } from "react-icons/bs";
 
 const Navbar = () => {
-  return <nav className="flex item-center justify-center w-full">Navbar</nav>;
+  return (
+    <main className="flex item-center justify-center w-full mt-3 text-white">
+      <nav className="bg-glassColor min-w-[200px] p-3  h-[65px] flex items-center justify-between border-0.5 border-[rgba(255, 255, 255, 0.5)] rounded-[18px]">
+        <div className="h-[40px] w-[40px] flex items-center justify-center rounded-[12px] bg-[#e3e3ff]">
+          <Image src={navImage} className="pt-1.5" height={40} width={40} />
+        </div>
+        <h1 className="text-[20px]">Jaydeep .</h1>
+        <button className="border-0 bg-transparent text-[26px] text-white">
+          <BsGridFill />
+        </button>
+      </nav>
+    </main>
+  );
 };
 
 export default Navbar;
